1e8df31b3330145445fe225db32516ff434da2ed
Colin FRISCH <colin.frisch@gmail.com>
2025-05-15 21:48:14 +0800

    fix

--- mesa_llm/llm_agent.py
+++ mesa_llm/llm_agent.py
@@ -3,10 +3,4 @@ from mesa_llm.module_llm import ModuleLLM
 
 class LLMAgent:
     def __init__(self, api_key: str, model: str = "openai/gpt-4o"):
-        self.llm = ModuleLLM(api_key = api_key, model = model)
-
-
-
-
-
-
+        self.llm = ModuleLLM(api_key=api_key, model=model)
--- mesa_llm/module_llm.py
+++ mesa_llm/module_llm.py
@@ -1,36 +1,34 @@
-from litellm import completion
 import os
 
+from litellm import completion
 
 
 class ModuleLLM:
     """
     Currently supports OpenAI, Anthropic, xIA, Huggingface, Ollama, OpenRouter, NovitaAI
     """
-    def __init__(self, api_key: str, model : str = None):
-        
+
+    def __init__(self, api_key: str, model: str):
         self.api_key = api_key
-        provider = model.split('/')[0].upper()
+        provider = model.split("/")[0].upper()
 
         os.environ[f"{provider}_API_KEY"] = self.api_key
-    
 
     def generate(self, prompt: str) -> str:
         response = completion(
-            model="openai/gpt-4o",
-            messages=[{ "content": prompt,"role": "user"}]
-            )
+            model="openai/gpt-4o", messages=[{"content": prompt, "role": "user"}]
+        )
         return response
-
 
 
 # test the module
 if __name__ == "__main__":
     from dotenv import load_dotenv
+
     load_dotenv()
 
-    api_key = os.getenv("OPENAI_API_KEY") # Or simply your API key
-    llm = ModuleLLM(api_key,"openai/gpt-4o")
-    
+    api_key = os.getenv("OPENAI_API_KEY")  # Or simply your API key
+    llm = ModuleLLM(api_key, "openai/gpt-4o")
+
     response = llm.generate("Hello, how are you?")
     print(response.choices[0].message.content)
