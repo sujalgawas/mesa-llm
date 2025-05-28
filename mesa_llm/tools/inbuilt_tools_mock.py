34153218d301a9ddcdc10f1142c276b07c8796ed
sanika <nair.sanika.pradeep@gmail.com>
2025-05-28 19:28:07 +0530

    added inbuilt tool for discussion

--- mesa_llm/tools/inbuilt_tools_mock.py
+++ mesa_llm/tools/inbuilt_tools_mock.py
@@ -2,6 +2,8 @@ import inspect
 import sys
 from typing import TYPE_CHECKING
 
+from mesa_llm.llm_agent import LLMAgent
+
 if TYPE_CHECKING:
     from mesa_llm.llm_agent import LLMAgent
 
@@ -21,18 +23,22 @@ def move_to_location(agent: "LLMAgent", target_coordinates: tuple[float, float])
 
 
 def speak_to(
-    speaker_agent: "LLMAgent", listener_agent: "LLMAgent", message: str
+    speaker_agent: LLMAgent, listener_agents: list[LLMAgent], message: str
 ) -> str:
-    """Speak to a given agent by updating the discussion object shared by both agents
-    Args:
-        speaker_agent: The agent speaking
-        listener_agent: The agent listening
-        message: The message to speak
-    Returns:
-        A string indicating the message spoken
     """
-
-    return f"{speaker_agent.id} → {listener_agent.id} : {message}"
+    Send a message to the recipients.
+    """
+    for recipient in [*listener_agents, speaker_agent]:
+        recipient.memory.add_to_memory(
+            type="Message",
+            content=message,
+            step=speaker_agent.model.steps,
+            metadata={
+                "sender": speaker_agent,
+                "recipients": listener_agents,
+            },
+        )
+    return f"{speaker_agent} → {listener_agents} : {message}"
 
 
 # Get all the functions in the module that are not private into a list
