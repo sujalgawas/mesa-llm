61d7fd655af1ff9d025fe781de626e8407a84c62
sanika <nair.sanika.pradeep@gmail.com>
2025-05-28 12:16:19 +0530

    added generate_obs function

--- mesa_llm/llm_agent.py
+++ mesa_llm/llm_agent.py
@@ -1,10 +1,19 @@
 from mesa.agent import Agent
+from mesa.discrete_space import (
+    OrthogonalMooreGrid,
+    OrthogonalVonNeumannGrid,
+)
 from mesa.model import Model
+from mesa.space import (
+    ContinuousSpace,
+    MultiGrid,
+    SingleGrid,
+)
 
 from mesa_llm import Plan
 from mesa_llm.memory import Memory
 from mesa_llm.module_llm import ModuleLLM
-from mesa_llm.reasoning import Reasoning
+from mesa_llm.reasoning import Observation, Reasoning
 from mesa_llm.tools.tool_manager import ToolManager
 
 
@@ -13,10 +22,10 @@ class LLMAgent(Agent):
     LLMAgent manages an LLM backend and optionally connects to a memory module.
 
     Parameters:
+        model (Model): The mesa model the agent in linked to.
         api_key (str): The API key for the LLM provider.
-        model (str): The model to use for the LLM in the format 'provider/model'. Defaults to 'openai/gpt-4o'.
+        llm_model (str): The model to use for the LLM in the format 'provider/model'. Defaults to 'openai/gpt-4o'.
         system_prompt (str | None): Optional system prompt to be used in LLM completions.
-        memory (Memory | None): Optional memory instance to attach to this agent. Can only be set once.
         reasoning (str): Optional reasoning method to be used in LLM completions.
 
     Attributes:
@@ -32,6 +41,8 @@ class LLMAgent(Agent):
         reasoning: type[Reasoning],
         llm_model: str = "openai/gpt-4o",
         system_prompt: str | None = None,
+        vision: float | None = None,
+        internal_state: list[str] | str | None = None,
     ):
         super().__init__(model=model)
 
@@ -48,8 +59,16 @@ class LLMAgent(Agent):
         )
 
         self.tool_manager = ToolManager()
-
+        self.vision = vision
         self.reasoning = reasoning(agent=self)
+        self.system_prompt = system_prompt
+
+        if isinstance(internal_state, str):
+            internal_state = [internal_state]
+        elif internal_state is None:
+            internal_state = []
+
+        self.internal_state = internal_state
 
     def apply_plan(self, plan: Plan) -> list[dict]:
         """
@@ -60,3 +79,47 @@ class LLMAgent(Agent):
             type="Tool_Call_Responses", content=str(tool_call_resp), step=plan.step
         )
         return tool_call_resp
+
+    def generate_obs(self) -> Observation:
+        """
+        Returns an instance of the Observation dataclass enlisting everything the agent can see in the model in that step.
+
+        If the agents vision is set to anything above 0, the agent will get the details of all agents falling in that radius.
+        If the agents vision is set to -1, then the agent will get the details of all the agents present in the simulation at that step.
+        If it is set to 0 or None, then no information is returned to the agent.
+
+        """
+        gen_obs = Observation()
+        gen_obs.step = self.model.steps
+        gen_obs.self_state = {
+            "system_prompt": self.system_prompt,
+            "location": self.pos,
+            "internal_state": self.internal_state,
+        }
+        if self.vision is not None and self.vision > 0:
+            if isinstance(
+                self.model.grid,
+                SingleGrid | MultiGrid | OrthogonalMooreGrid | OrthogonalVonNeumannGrid,
+            ):
+                neighbor_coords = self.model.grid.get_neighborhood(
+                    self.pos, moore=True, include_center=False, radius=self.vision
+                )
+                # To get agents at those coordinates:
+                neighbors = [self.model.grid[x, y] for x, y in neighbor_coords]
+            elif isinstance(self.model.space, ContinuousSpace):
+                neighbors, _ = self.get_neighbors_in_radius(radius=self.vision)
+        elif self.vision == -1:
+            all_agents = list(self.model.agents)
+            neighbors = [agent for agent in all_agents if agent is not self]
+        else:
+            neighbors = []
+
+        local_state = {}
+        for i in neighbors:
+            local_state[i.__class__.__name__ + " " + str(i.unique_id)] = {
+                "position": i.pos,
+                "internal_state": i.internal_state,
+            }
+        gen_obs.local_state = local_state
+
+        return gen_obs
