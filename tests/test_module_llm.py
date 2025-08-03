756f3924dab3ea78d86a754e5243fb0098426c9c
Colin FRISCH <colin.frisch@gmail.com>
2025-08-03 19:10:52 +0200

    tests : module LLM

---
+++ tests/test_module_llm.py
@@ -0,0 +1,119 @@
+import os
+from unittest.mock import patch
+
+import pytest
+
+from mesa_llm.module_llm import ModuleLLM
+
+
+@pytest.fixture
+def mock_api_key():
+    with patch.dict(os.environ, {"OPENAI_API_KEY": "test_key"}, clear=True):
+        yield
+
+
+class TestModuleLLM:
+    """Test ModuleLLM class"""
+
+    def test_module_llm_initialization(self, mock_api_key):
+        # Test initialization with default values
+        llm = ModuleLLM(llm_model="openai/gpt-4o")
+        assert llm.api_key == "test_key"
+        assert llm.api_base is None
+        assert llm.llm_model == "openai/gpt-4o"
+        assert llm.system_prompt is None
+
+        # Test initialization with ollama provider
+        llm = ModuleLLM(llm_model="ollama/llama2")
+        assert llm.api_base == "http://localhost:11434"
+        assert llm.llm_model == "ollama/llama2"
+        assert llm.system_prompt is None
+
+        # Test initialization with ollama provider + custom api_base
+        llm = ModuleLLM(llm_model="ollama/llama2", api_base="http://localhost:99999")
+        assert llm.api_base == "http://localhost:99999"
+        assert llm.llm_model == "ollama/llama2"
+        assert llm.system_prompt is None
+
+        # Test init without api_key in dotenv
+        with patch.dict(os.environ, {}, clear=True), pytest.raises(ValueError):
+            ModuleLLM(llm_model="openai/gpt-4o")
+
+    def test_get_messages(self):
+        # Test get_messages with string prompt
+        llm = ModuleLLM(llm_model="openai/gpt-4o")
+        messages = llm.get_messages("Hello, how are you?")
+        assert messages == [
+            {"role": "system", "content": None},
+            {"role": "user", "content": "Hello, how are you?"},
+        ]
+
+        # Test get_messages with list of prompts
+        messages = llm.get_messages(
+            ["Hello, how are you?", "What is the weather in Tokyo?"]
+        )
+        assert messages == [
+            {"role": "system", "content": None},
+            {"role": "user", "content": "Hello, how are you?"},
+            {"role": "user", "content": "What is the weather in Tokyo?"},
+        ]
+
+        # Test get_messages with system prompt
+        llm = ModuleLLM(
+            llm_model="openai/gpt-4o", system_prompt="You are a helpful assistant."
+        )
+        messages = llm.get_messages("Hello, how are you?")
+        assert messages == [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": "Hello, how are you?"},
+        ]
+
+        # Test get_messages with system prompt and list of prompts
+        messages = llm.get_messages(
+            ["Hello, how are you?", "What is the weather in Tokyo?"]
+        )
+        assert messages == [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": "Hello, how are you?"},
+            {"role": "user", "content": "What is the weather in Tokyo?"},
+        ]
+
+        # Test get_messages no system prompt and no prompt
+        messages = llm.get_messages(prompt=None)
+        assert messages == [{"role": "system", "content": None}]
+
+    def test_generate(self):
+        # Test generate with string prompt
+        llm = ModuleLLM(llm_model="openai/gpt-4o")
+        response = llm.generate(prompt="Hello, how are you?")
+        assert response is not None
+
+        # Test generate with list of prompts
+        response = llm.generate(
+            prompt=["Hello, how are you?", "What is the weather in Tokyo?"]
+        )
+        assert response is not None
+
+        # Test generate with string prompt
+        llm = ModuleLLM(llm_model="ollama/llama2")
+        response = llm.generate(prompt="Hello, how are you?")
+        assert response is not None
+
+        # Test generate with list of prompts
+        response = llm.generate(
+            prompt=["Hello, how are you?", "What is the weather in Tokyo?"]
+        )
+        assert response is not None
+
+    @pytest.mark.asyncio
+    async def test_agenerate(self):
+        # Test agenerate with string prompt
+        llm = ModuleLLM(llm_model="openai/gpt-4o")
+        response = await llm.agenerate(prompt="Hello, how are you?")
+        assert response is not None
+
+        # Test agenerate with list of prompts
+        response = await llm.agenerate(
+            prompt=["Hello, how are you?", "What is the weather in Tokyo?"]
+        )
+        assert response is not None
